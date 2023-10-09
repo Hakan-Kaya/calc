aa282f00f9ece71bba8fa3465f81552a90990453
Hakan Kaya <hakaya@outlook.com>
2023-10-09 12:44:31 +0300

    Initial Commit

---
+++ Calc/CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp.swift
+//  Calc
+//
+//  Created by Hakan Kaya on 9.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
