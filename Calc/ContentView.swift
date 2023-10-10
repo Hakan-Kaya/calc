6f5a4c6f60adcd5331133a73529330811846cb9e
Hakan Kaya <hakaya@outlook.com>
2023-10-10 13:00:26 +0300

    Design changes

--- Calc/ContentView.swift
+++ Calc/ContentView.swift
@@ -15,30 +15,40 @@ struct ContentView: View {
     
     var body: some View {
         HStack {
-            Text("Result: ")
             Text("\(currentNumber)")
+                .font(.title)
+                .frame(width: 300, height: 50, alignment: .trailing)
+                .border(Color.black)
         }
         .bold()
-        .padding()
+        .padding(.all)
         
         VStack {
-            HStack {
+            HStack(spacing: 20) {
                 NumberButton(currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonValue: "1")
                 NumberButton(currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonValue: "2")
                 NumberButton(currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonValue: "3")
                 NumberButton(currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonValue: "4")
-                NumberButton(currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonValue: "5")
+                
             }
+            .padding()
         
-            HStack {
+            HStack(spacing: 20) {
+                NumberButton(currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonValue: "5")
                 NumberButton(currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonValue: "6")
                 NumberButton(currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonValue: "7")
                 NumberButton(currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonValue: "8")
+            }
+            .padding()
+            
+            HStack(spacing: 20) {
                 NumberButton(currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonValue: "9")
                 NumberButton(currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonValue: "0")
+                NumberButton(currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonValue: ".")
             }
+            .padding()
             
-            HStack {
+            HStack(spacing: 20) {
                 OperandButton(previousNumber: $previousNumber, currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonOperand: "+", calculate: $calculate)
                 OperandButton(previousNumber: $previousNumber, currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonOperand: "-", calculate: $calculate)
                 OperandButton(previousNumber: $previousNumber, currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonOperand: "*", calculate: $calculate)
@@ -46,6 +56,8 @@ struct ContentView: View {
                 OperandButton(previousNumber: $previousNumber, currentNumber: $currentNumber, enteringNewNumber: $enteringNewNumber, buttonOperand: "=", calculate: $calculate)
             }
         }
+        .padding()
+        Spacer()
     }
 }
 
